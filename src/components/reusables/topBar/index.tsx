d3f86ac7be3f4df37773d89a02ad6eab359673be
Dorin <turturica.dorin33@gmail.com>
2024-02-02 14:13:58 +0200

    add prittier

--- src/components/reusables/topBar/index.tsx
+++ src/components/reusables/topBar/index.tsx
@@ -12,57 +12,57 @@ const TopBar = () => {
   }
 
   return (
-    <div className="flex items-center shadow-sm w-full p-4">
-      <Drawer className="md:hidden mx-2" />
+    <div className="flex w-full items-center p-4 shadow-sm">
+      <Drawer className="mx-2 md:hidden" />
 
-      <div className="flex justify-between items-center">
+      <div className="flex items-center justify-between">
         <SearchBarChipTopbar
           placeholder="Discover work, badges & skills"
           handleOnChange={handleOnSeachChange}
         />
 
-        <div className="hidden md:flex items-center">
+        <div className="hidden items-center md:flex">
           <div className="relative cursor-pointer py-2">
             <img
               src="/calendar.svg"
               alt="calendarIcon"
-              className="h-5 w-5 mx-3"
+              className="mx-3 h-5 w-5"
             />
           </div>
           <div className="relative cursor-pointer py-2">
             <img
               src="/sms.svg"
               alt="smsIcon"
-              className="h-5 w-5 mx-3 cursor-pointer"
+              className="mx-3 h-5 w-5 cursor-pointer "
             />
-            <NotificationCountChip count={8} className="top-0 right-0" />
+            <NotificationCountChip count={8} className="right-0 top-0" />
           </div>
 
-          <div className="relative py-2 cursor-pointer mr-3 ">
+          <div className="relative mr-3 cursor-pointer py-2 ">
             <img
               src="/notification.svg"
               alt="notificationIcon"
-              className="h-5 w-5 mx-3 cursor-pointer"
+              className="mx-3 h-5 w-5 cursor-pointer"
             />
-            <NotificationCountChip count={23} className="top-0 right-0" />
+            <NotificationCountChip count={23} className="right-0 top-0" />
           </div>
 
-          <div className="flex items-center cursor-pointer">
+          <div className="flex cursor-pointer items-center">
             <img
               src="/avatar.png"
               alt="AvatarIcon"
-              className="h-10 w-10 mx-2 rounded-full"
+              className="mx-2 h-10 w-10 rounded-full"
             />
             <div className="flex flex-col">
               <span className="mx-2 text-sm font-semibold">Theo Edwards</span>
-              <span className="mx-2 text-sm text-mediumGrey">
+              <span className="text-mediumGrey mx-2 text-sm">
                 Administrator
               </span>
             </div>
             <img
               src="/arrowSquareDown.svg"
               alt="ArrowSquareDownIcon"
-              className="h-5 w-5 mx-2"
+              className="mx-2 h-5 w-5"
             />
           </div>
         </div>
