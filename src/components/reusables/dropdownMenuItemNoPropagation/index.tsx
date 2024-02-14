30de8cc9f330bf10a04a37a75660c59cca219ada
Dorin <turturica.dorin33@gmail.com>
2024-02-15 00:50:37 +0200

    add onClick to DropdownMenuItemNoPropagation

--- src/components/reusables/dropdownMenuItemNoPropagation/index.tsx
+++ src/components/reusables/dropdownMenuItemNoPropagation/index.tsx
@@ -3,14 +3,16 @@ import { DropdownMenuItem } from "@/components/ui/dropdown-menu";
 type DropdownMenuItemNoPropagationProps = {
   className?: string;
   children?: React.ReactNode;
+  onClick?: (e: React.MouseEvent) => void;
 };
 
 export function DropdownMenuItemNoPropagation({
   className,
   children,
+  onClick,
 }: DropdownMenuItemNoPropagationProps) {
   return (
-    <DropdownMenuItem className={`${className}`}>
+    <DropdownMenuItem className={`${className}`} onClick={onClick}>
       <div
         onClick={(e) => {
           e.stopPropagation();
